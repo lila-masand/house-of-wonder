656348be13bad1cec339546889215db93fd9e61d
lila-masand <lmasand@umass.edu>
2023-12-05 00:03:19 -0500

    Added a Level 1 scene and started building. Will link to the door of the prototype level to give a better sense of the level flow.

---
+++ Assets/MovingPlatformVertical.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class MovingPlatformVertical : MonoBehaviour
+{
+    //could maybe say that if target isn't null, raycast to target instead
+    public GameObject target;
+    public float velocity = 5f;
+    public bool PlayerOn;
+
+    //for debugging
+    //public float raytarget;
+
+    private bool away = true;
+    private Vector3 origin;
+    private RaycastHit hit;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        origin = transform.position;
+
+        // It will start by moving away from its origin 
+        Physics.Raycast(origin, new Vector3(0f, -1f, 0f), out hit, Mathf.Infinity);
+        PlayerOn = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (away && transform.position.y < hit.point.y + 1.5)
+        {
+   
+            Physics.Raycast(transform.position, new Vector3(0f, 1f, 0f), out hit, Mathf.Infinity);
+
+            away = false;
+        }
+
+        else if (!away && transform.position.y > hit.point.y - 1.5)
+        {
+            Physics.Raycast(transform.position, new Vector3(0f, -1f, 0f), out hit, Mathf.Infinity);
+
+            away = true;
+        }
+
+        float step = velocity*Time.deltaTime;
+
+        transform.position = Vector3.MoveTowards(transform.position, hit.point, step);
+      
+
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            // need overall level script that has a variable tracking Thyra's last position?
+
+            PlayerOn = true;
+        }
+
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            // need overall level script that has a variable tracking Thyra's last position?
+
+            PlayerOn = false;
+
+        }
+
+    }
+}
